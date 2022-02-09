20ed02b162317ecdbfc9d21146c3ca7ad290376b
VeenaHT <Veenaht@gmail.com>
2022-02-09 15:54:03 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World :web hook ");
+    System.out.println("Hello World :web hook2 ");
     }
 }
